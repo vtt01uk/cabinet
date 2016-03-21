c696ece6771d4b34e2cf215f190061cb6934708a
vtt01uk <vtt01uk@gmail.com>
2016-03-21 14:35:14 +0000

    Show page works

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,32 +1,46 @@
 class DocsController < ApplicationController
-
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+  
   def index
   end
   
   def create
+    #create a new document will title and text
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
+  end
+    
+  def show
+
   end
   
   def new
+    @doc = Doc.new
   end
   
   def edit
   end
-  
-  def show
-  end
+
   
   def update
   end
   
   def destroy
+    
   end
   
   # private methods apply to these controllers; 
-  # helps with reducing redundancy
+  # helps with reducing redundancy (DRY methodology)
   private
     def find_doc
+      @doc = Doc.find(params[:id])
     end
 
     def doc_params
+      params.require(:doc).permit(:title, :content)
     end
 end
