9029cc691e08ff870b37773e707ab3506316f58e
vtt01uk <vtt01uk@gmail.com>
2016-03-21 13:52:41 +0000

    Added Doc controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,32 @@
+class DocsController < ApplicationController
+
+  def index
+  end
+  
+  def create
+  end
+  
+  def new
+  end
+  
+  def edit
+  end
+  
+  def show
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  # private methods apply to these controllers; 
+  # helps with reducing redundancy
+  private
+    def find_doc
+    end
+
+    def doc_params
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
+  #relating to the controller Docs, generates the necessary routes
+  resources :docs
 end
